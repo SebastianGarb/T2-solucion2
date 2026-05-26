9d7750e2426d44c02c175e44156542f887aca04a
“SebastianGarb” <sebastiangarboza63@hotmail.com>
2026-05-25 19:07:15 -0500

    version2-solucion2

--- solucion2.py
+++ solucion2.py
@@ -1,15 +1,15 @@
-def suma_entre_posiciones(lst, pi, pf):
+def sumaentreposiciones(lst, pi, pf):
     if pi > pf:
         return 0
     else:
-        return lst[pi-1] + suma_entre_posiciones(lst, pi+1, pf)
+        return lst[pi-1] + sumaentreposiciones(lst, pi+1, pf)
 
-texto_lista = input("Ingrese los números de la lista separados por espacios: ")
-lista = [int(num) for num in texto_lista.split()]
+txtlista = input("Ingrese los números de la lista separados por espacios: ")
+lista = [int(num) for num in txtlista.split()]
 
 PI = int(input("Ingrese la posición inicial: "))
 PF = int(input("Ingrese la posición final: "))
 
-resultado = suma_entre_posiciones(lista, PI, PF)
+resultado = sumaentreposiciones(lista, PI, PF)
 print(f"\nLista ingresada: {lista}")
 print(f"Resultado = {resultado}")
